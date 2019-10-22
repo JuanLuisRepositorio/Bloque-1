be3939c4fffccee9660a06df0ea250deb2293554
ProjectSupervisaPrivate <55844039+ProjectSupervisaPrivate@users.noreply.github.com>
2019-10-22 10:49:03 +0200

    Create code.py

---
+++ List/code.py
@@ -0,0 +1,17 @@
+#Listas son como arreglos flexibles. Estos contienen variables que desees y todas los tipos de variables que esten soportados. Los elementos de la lista pueden ser iterated over! (se explicará mas adelante en el tutorial) o accediendo a ellos via un indice basado en cero.
+
+
+milista = []
+milista.append(1)
+milista.append(2)
+milista.append(3)
+print milista[0] # imprime 1
+print milista[1] # imprime 2
+print milista[2] # imprime 3
+
+# prints out 1,2,3
+for x in mylist:
+    print x
+    
+mylist = [1,2,3]
+print mylist[10]    

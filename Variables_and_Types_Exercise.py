5b765812989845a44a97ac8e81c232a32032e7eb
ProjectSupervisaPrivate <55844039+ProjectSupervisaPrivate@users.noreply.github.com>
2019-10-22 10:27:40 +0200

    Create Variables_and_Types_Exercise.py

---
+++ Variables_and_Types_Exercise.py
@@ -0,0 +1,10 @@
+# Escribe tu propio codigo aqui
+
+
+# probando el codigo
+if micadena == "hola":
+    print "Cadena: %s" % micadena
+if isinstance(miflotante, float) and miflotante == 10.0:
+    print "Flotante: %d" % miflotante
+if isinstance(miIntegral, int) and miIntegral == 20:
+    print "Integral: %d" % miIntegral

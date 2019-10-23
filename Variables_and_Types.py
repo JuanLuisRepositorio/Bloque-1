fb5a726f03ec012adba614def69410f478a83387
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-10-23 17:06:09 +0200

    Update Variables_and_Types.py

--- Variables_and_Types.py
+++ Variables_and_Types.py
@@ -1,6 +1,12 @@
 #print nos sirve para imprimir la variable o texto que queramos, nos ayuda a conocer el conetenido de las variables o a mostrar resultados
 print("Hola mundo")
 
+#Las variables son contenedores para almacenar datos, una variable se crea en el momento en el que le damos un valor
+x = 5
+y = "John"
+print(x)
+print(y)
+
 #Numeros
 #Python soporta dos tipos de números - integrales y numeros de punto flotante. (Tambien soporta numeros complejos, los cuales no se explicarán en este tutorial).
 #Para definir un integral, usa la siguiente sintaxis:

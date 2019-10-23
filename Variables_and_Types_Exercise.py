3b5700fd702f7fb1a8aaacc73ff35936f4c00544
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-10-23 16:40:43 +0200

    Update Variables_and_Types_Exercise.py

--- Variables_and_Types_Exercise.py
+++ Variables_and_Types_Exercise.py
@@ -1,3 +1,7 @@
+#El objetivo de este ejercicio es crear una cadena de caracteres, un entero y un numero real. La variable de tipo cadena de caracteres debe contener la palabra
+# "hola", la variable de tipo real debe contener el numero 10 y la variable de numero entero debe contener el numero 20
+#si hemos completado el ejercicio de forma satisfactoria apareceran en la consola los tres valores
+
 # Escribe tu propio codigo aqui
 
 

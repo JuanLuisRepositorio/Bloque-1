894b2e3d4385aa5067251fb7087f2362d556656b
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-10-23 16:26:08 +0200

    Update Variables_and_Types.py

--- Variables_and_Types.py
+++ Variables_and_Types.py
@@ -1,46 +1,41 @@
-#Números
-#Python soporta dos tipos de números - integrales y numeros de punto flotante. (Tambien soporta números complejos, los cuales no se explicarán en este tutorial).
-
+#Numeros
+#Python soporta dos tipos de números - integrales y numeros de punto flotante. (Tambien soporta numeros complejos, los cuales no se explicarán en este tutorial).
 #Para definir un integral, usa la siguiente sintaxis:
-
 myint = 7
-#Para definir un número de punto flotante, debes usar una de las siguientes notaciones:
 
+#Para definir un numero de punto flotante, debes usar una de las siguientes notaciones:
 midecimal = 7.0
 midecimal = float(7)
 
+#Cadenas
 #Las cadenas están definidas con comillas sencillas o compuestas.
 
 micadena = 'Hola'
 micadena = "Hola"
+
 #La diferencia ente las dos es que usando doble comillas lo hace mas facil de incluir los apostofres (de lo contrario concluirira la cadena si se usa doble comillas)
-
 micadena = "No te preocupes de los 'apostofres' usando comillas dobles"
-#There are additional variations on defining strings that make it easier to include things such as carriage returns, backslashes and Unicode characters. These are beyond the scope of this tutorial, but are covered in the Python documentation.
 
-#To combine both text and a variable, Python uses the + character:
-x = "awesome"
-print("Python is " + x)
-
-#For numbers, the + character works as a mathematical operator:
-uno = 1
-dos = 2
-tres = uno + dos
-print(tres)
-
-#You can also use the + character to add a variable to another variable:
+#Combinar variables
+#Podemos usar el caracter + para anadir una variable a otra:
 x = "Python is "
 y = "awesome"
 z =  x + y
 print(z)
 
-hola = "hola"
-mundo = "mundo"
-holamundo = hola + " " + mundo
+#Tambien podemos usarlo para juntar variables y caracteres:
+x = "awesome"
+print("Python is " + x)
+
+#En el caso de variables de tipo numeríco, + seria tratado como un operador matematico:
+x = 1
+y = 2
+z = x + y
+print(z)
+
 #Se puede asignar a mas de una variable simultaneamente en la misma linea, como se muestra aquí
-
 a, b = 3, 4
-#Mezclando operadores entre los numeros y cadenas que no son soportadas:
 
+#Mezclando operadores entre los numeros y cadenas que no son soportadas:
 # Esto no funcionará!
-print uno + dos + hola
+print (x + y + hola)

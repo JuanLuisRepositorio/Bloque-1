bafbcc2fd5bcfe1bf96e5464ce2de8c4d1035f4e
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-10-24 15:49:48 +0200

    Create otros_operadores_ejercicio.py

---
+++ otros_operadores_ejercicio.py
@@ -0,0 +1,11 @@
+#isnot funciona alreves que is
+#Comprueba si un valor se encuentra en un listado
+#Cambia los valores para obtener resultado True
+
+x = 6
+y = -3
+z = 4
+
+
+resultado = ((x>y)|(x==z))&(x<=y)
+print(resultado)

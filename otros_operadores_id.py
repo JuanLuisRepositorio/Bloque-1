f42e34e8e6a824085acf1d08fa97d57c4d13ec4e
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-10-24 13:13:04 +0200

    Update otros_operadores_id.py

--- otros_operadores_id.py
+++ otros_operadores_id.py
@@ -1,7 +1,11 @@
 #operadores de identidad
+#is.
+#si dos variables son dienticas devuelve True si no devuelve False
+x = "hola"
+y = "hola"
 
-resultado = "hola" is "hola"
-print()
+resultado = x is y
+print(resultado)
 
 #diferencia entre is y ==
 x = 2
@@ -12,3 +16,12 @@ print(resultado)
 
 resultado = x is y
 print(resultado)
+
+#operador de membresia
+#in 
+#Comprueba si un valor se encuentra en un listado
+pets=['dog','cat','ferret']
+resultado = 'fox' in pets
+print(resultado)
+
+#operadores logicos

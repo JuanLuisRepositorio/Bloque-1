e2ad896d37390333729611b95a6525a340ac9dad
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-10-24 13:00:54 +0200

    Create otros_operadores_id.py

---
+++ otros_operadores_id.py
@@ -0,0 +1,14 @@
+#operadores de identidad
+
+resultado = "hola" is "hola"
+print()
+
+#diferencia entre is y ==
+x = 2
+y = 2.0
+
+resultado = x==y
+print(resultado)
+
+resultado = x is y
+print(resultado)

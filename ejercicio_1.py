fe7cb2cb66ba87013de7c2564ac9b81fccfd3af3
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-10-25 09:16:47 +0200

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,18 +1,21 @@
 #ejercicio 1
 
-#anade a la lista de cadenas las palabras "orange", "apple" y "banana" en este orden y elimina el elmento "audi"
+#anade a la lista "frutas" las palabras "orange", "apple" y "banana" en este orden y elimina el elmento "audi"
 
-lista = ["lemon","audi"]
+frutas = ["lemon","audi"]
+
+
+
 
 #Codigo de comprobacion
 a = 20
-if (lista[0] == "lemon")and(lista[1]=="orange")and(lista[2]=="apple")and(lista[3]=="banana"):
+if (frutas[0] == "lemon")and(frutas[1]=="orange")and(frutas[2]=="apple")and(frutas[3]=="banana"):
   a = 1
-elif(lista[0] == "lemon")and(lista[1]=="orange")and(lista[2]=="apple")and(lista[3]=="banana"):
+elif(frutas[0] == "lemon")and(frutas[1]=="orange")and(frutas[2]=="apple")and(frutas[3]=="banana"):
   a = 2
-elif(lista[0] == "lemon")and(lista[1]=="orange")and(lista[2]=="apple")and(lista[3]=="banana"):
+elif(frutas[0] == "lemon")and(frutas[1]=="orange")and(frutas[2]=="apple")and(frutas[3]=="banana"):
   a = 3
-elif(lista[0] == "lemon")and(lista[1]=="orange")and(lista[0]=="apple")and(lista[3]=="banana"):
+elif(frutas[0] == "lemon")and(frutas[1]=="orange")and(frutas[0]=="apple")and(frutas[3]=="banana"):
   a = 4
 
 print("El resultado final para el formulario es: " + str(a))

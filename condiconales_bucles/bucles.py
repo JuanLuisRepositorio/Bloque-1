d8d12a465cc6265fead4b8e421824ea051faee1d
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-08 11:03:07 +0100

    Create bucles.py

---
+++ condiconales_bucles/bucles.py
@@ -0,0 +1,16 @@
+#Ejemplo 3
+#bucle for 1
+
+print("Número de participantes de los cuatro primeros trimestres")
+for i in [32, 35, 14, 16]:
+    print("Participantes: "+ str(i))
+
+
+
+#bucle for 2
+print("Saludamos a los 6 compañeros nuevos")
+for i in range(1, 6):
+    print("Hola compañero: "+ str(i))    
+
+print("Gracias a todos por venir")
+

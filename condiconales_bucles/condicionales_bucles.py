8ffea269545f2f6fc97302bcdaa17365a7f6aa73
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-11 12:17:26 +0100

    Create condicionales_bucles.py

---
+++ condiconales_bucles/condicionales_bucles.py
@@ -0,0 +1,6 @@
+#Ejemplo 5
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9
+,10, 11, 12, 13, 14] #Creamos la lista con números
+for num in numeros: #En la variable "num" almacenamos los ítem de la lista
+    if num % 2 == 0: #Condición: Si el resto de la división por dos es cero entonces:
+        print (num) # Imprimimos la variable num

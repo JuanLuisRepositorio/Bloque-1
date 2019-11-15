6ca3d4ea37e9b8d9c1670157f5b3e98a7c8cf7ff
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-15 09:38:56 +0100

    Update Ejercicio_bucles_1.py

--- condiconales_bucles/Ejercicio_bucles_1.py
+++ condiconales_bucles/Ejercicio_bucles_1.py
@@ -1,7 +1,5 @@
-#Ejemplo 3
+#Ejercicio bucles 3
 #bucle for 1
-#Completa este codigo para que muestre los números del 1 al 10 en pantalla de la forma "Numero: " y el numero en cuestion
+#Crea un codigo que muestre los números del 1 al 10 en pantalla de la forma "Numero: " y el numero en cuestion
 
-
-for i in range(1, 10):
     

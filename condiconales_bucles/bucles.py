22995081cd3b6b6907b70aaeb7018667d76f5d53
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-15 10:15:08 +0100

    Update bucles.py

--- condiconales_bucles/bucles.py
+++ condiconales_bucles/bucles.py
@@ -24,8 +24,9 @@ while i <= 3:
 print("Programa terminado")
 
 #bucle while 2
-i = 1
+i = int(input("introduce numero para empezar el bucle: "))
 while i <= 50:
     print(i)
-    i = 3 * i + 1
+    i += 1
 print("Programa terminado")
+

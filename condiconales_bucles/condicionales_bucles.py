0fd364e8403b6e76a24409b77287de6655f7bfc1
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-18 17:27:54 +0100

    Update condicionales_bucles.py

--- condiconales_bucles/condicionales_bucles.py
+++ condiconales_bucles/condicionales_bucles.py
@@ -1,7 +1,12 @@
-#Ejemplo 5
+#Ejemplo 9
 #Este ejemplo nos devuelve los numeros que son divisibles entre 2 de forma exacta
+encontrado = False
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9
 ,10, 11, 12, 13, 14] #Creamos la lista con números
 for num in numeros: #En la variable "num" almacenamos los ítem de la lista
-    if num % 2 == 0: #Condición: Si el resto de la división por dos es cero entonces:
+    if num % 4 == 0: #Condición: Si el resto de la división por
         print (num) # Imprimimos la variable num
+        encontrado = True
+    if encontrado == False:
+        print("Seguimos")
+        

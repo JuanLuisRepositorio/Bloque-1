546832cab32b15432f2ef24ecbb45ddacc67817e
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-18 16:47:52 +0100

    Update bucles.py

--- condiconales_bucles/bucles.py
+++ condiconales_bucles/bucles.py
@@ -7,13 +7,17 @@ for i in range(1, 10):
 #Ejemplo 7
 print("Número de participantes de los cuatro primeros trimestres")
 a = 1
-for i in [32, 35, 14, 16]:
-    print("En el "+ str(a) + "º semestre tenemos "+ str(i) + " participantes")
+for numero in [32, 35, 14, 16]:
+    print("En el "+ str(a) + "º semestre tenemos "+ str(numero) + " participantes")
     a += 1
     
 print("¿En que paises tenemos oficinas?")
-for i in ["Chile", "Ecuador", "Peru", "Argentina", "España", "Brasil"]:
+for paises in ["Chile", "Ecuador", "Peru", "Argentina", "España", "Brasil"]:
     print("Tenemos oficina en: "+ str(i))
+
+paises = ["Chile", "Ecuador", "Peru", "Argentina", "España", "Brasil"]
+for i in range(len(paises)):
+    print("Tenemos oficina en: "+ str(paises[i]))    
 
 #Ejemplo 8
 #bucle while 1

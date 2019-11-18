f89855430a819a0413e1d896ee20caa62ed89f2f
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-18 14:07:08 +0100

    Update condicionales_bucles.py

--- condiconales_bucles/condicionales_bucles.py
+++ condiconales_bucles/condicionales_bucles.py
@@ -1,4 +1,5 @@
 #Ejemplo 5
+#Este ejemplo nos devuelve los numeros que son divisibles entre 2 de forma exacta
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9
 ,10, 11, 12, 13, 14] #Creamos la lista con números
 for num in numeros: #En la variable "num" almacenamos los ítem de la lista

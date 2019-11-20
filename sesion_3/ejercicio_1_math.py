aedaada3171b0b34921807e961a0aa2ba22e52eb
JuanLuisRepositorio <55844039+JuanLuisRepositorio@users.noreply.github.com>
2019-11-20 10:16:41 +0100

    Create ejercicio_1_math.py

---
+++ sesion_3/ejercicio_1_math.py
@@ -0,0 +1,7 @@
+#Utiliza la librería “math” de Python para realizar los siguientes cálculos sobre el número 18.5
+
+#seno
+#exponencial
+#logaritmo
+
+#Muestra los resultados en líneas independientes como en el ejemplo anterior
